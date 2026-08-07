d23f5f46d1fcfd166b1a06cf3581af9aee203034
Felix Holfelder <felixholfelder1@t-online.de>
2026-08-07 15:06:47 +0200

    formatting

--- app/models/live-meeting.ts
+++ app/models/live-meeting.ts
@@ -1,10 +1,10 @@
 export interface LiveMeeting {
-    team_home: string;
-    team_guest: string;
-    games_home: number;
-    games_guest: number;
+  team_home: string;
+  team_guest: string;
+  games_home: number;
+  games_guest: number;
 }
 
 export interface LiveMeetingsResponse {
-    data: LiveMeeting;
+  data: LiveMeeting;
 }
