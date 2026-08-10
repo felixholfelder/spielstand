f38df1fa6549ce485841667f909908ea805d982f
Felix Holfelder <felixholfelder1@t-online.de>
2026-08-10 20:53:29 +0200

    added button to toggle fullscreen for meetings

--- app/layouts/meeting.vue
+++ app/layouts/meeting.vue
@@ -2,10 +2,47 @@
 import { useTheme } from "vuetify";
 
 const theme = useTheme();
-
 const { increase, decrease } = useGamesCountFontSize();
 
-onMounted(() => {
+import { ref, onMounted, onBeforeUnmount } from 'vue'
+const isFullscreen = ref(false)
+
+function updateFullscreenState() {
+  const state = !!document.fullscreenElement
+  isFullscreen.value = state
+}
+async function toggleFullscreen() {
+  try {
+    if (!document.fullscreenElement) {
+      await enterFullscreen()
+      localStorage.setItem("isFullscreen", "1")
+    } else {
+      await exitFullscreen()
+      localStorage.setItem("isFullscreen", "0")
+    }
+  } catch (err) {
+    console.error('Fullscreen-Fehler:', err)
+  }
+}
+
+async function enterFullscreen() {
+  await document.documentElement.requestFullscreen()
+}
+
+async function exitFullscreen() {
+  await document.exitFullscreen()
+}
+
+onBeforeUnmount(() => {
+  document.removeEventListener('fullscreenchange', updateFullscreenState)
+})
+
+onMounted(async () => {
+  document.addEventListener('fullscreenchange', updateFullscreenState)
+  if (localStorage.getItem("isFullscreen") == "1") {
+    await enterFullscreen()
+  }
+
   if (import.meta.client) {
     const saved = localStorage.getItem("theme");
     if (saved) {
@@ -19,8 +56,10 @@ onMounted(() => {
   <v-app>
     <v-main>
       <div class="controls-row">
-        <v-icon-btn icon="mdi-arrow-left" @click="$router.back()" />
+        <v-icon-btn icon="mdi-arrow-left" @click="exitFullscreen(); $router.back()" />
         <v-spacer />
+        <v-icon-btn :icon="isFullscreen ? 'mdi-arrow-collapse' : 'mdi-arrow-expand'" @click="toggleFullscreen"></v-icon-btn>
+        <v-divider class="ma-2" vertical />
         <v-icon-btn icon="mdi-minus" @click="decrease" />
         <v-icon-btn icon="mdi-plus" @click="increase" />
       </div>
