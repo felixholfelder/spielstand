21cf996d1244294c4054788170068c97ea4a9666
Felix Holfelder <felixholfelder1@t-online.de>
2026-09-13 17:59:36 +0200

    added caching for clubs and meetings

--- server/api/teams.get.ts
+++ server/api/teams.get.ts
@@ -1,21 +1,30 @@
-export default defineEventHandler(async (event) => {
-  const config = useRuntimeConfig();
-  const query = getQuery(event);
+export default defineCachedEventHandler(
+  async (event) => {
+    const config = useRuntimeConfig();
+    const query = getQuery(event);
 
-  const association = query.association;
-  const season = "x";
-  const clubId = query.clubId;
-  const clubSlug = "x";
+    const association = query.association;
+    const season = "x";
+    const clubId = query.clubId;
+    const clubSlug = "x";
 
-  const result = await $fetch(
-    `${config.public.baseApiUrl}/click-tt/${association}/${season}/verein/${clubId}/${clubSlug}/mannschaften`,
-    {
-      method: "GET",
-      query: {
-        _data: "",
+    const result = await $fetch(
+      `${config.public.baseApiUrl}/click-tt/${association}/${season}/verein/${clubId}/${clubSlug}/mannschaften`,
+      {
+        method: "GET",
+        query: {
+          _data: "",
+        },
       },
-    },
-  );
+    );
 
-  return result;
-});
+    return result;
+  },
+  {
+    maxAge: 60 * 60 * 8, // 8 hours cache
+    getKey: (event) => {
+      const query = getQuery(event);
+      return `teams-${query.association}-${query.clubId}`;
+    },
+  },
+);
